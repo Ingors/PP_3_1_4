23e747b62702c9c91c0c928e8576d863258792c2
Admin-PC\Admin <itmo1652@mail.ru>
2022-10-14 16:54:02 +0300

    commit 2 10/14/2022

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,8 +1,5 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.security.core.userdetails.UserDetails;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,28 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import ru.kata.spring.boot_security.demo.service.UserService;
+import ru.kata.spring.boot_security.demo.model.User;
+
 
 
 @Controller
 @RequestMapping("/")
 public class UserController {
 
-    private final UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping("/user")
-    public String userPage(@AuthenticationPrincipal UserDetails userDetails, Model model) {
-        model.addAttribute("user", userService.getUserByEmail(userDetails.getUsername()));
-        return "user";
-    }
-
-    @GetMapping("/login")
-    public String loginPage() {
+    @GetMapping(value = "login")
+    public String showLoginPage() {
         return "login";
+    }
+
+    @GetMapping("user")
+    public String showUserInfo(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("user", user);
+        return "user";
     }
 }
